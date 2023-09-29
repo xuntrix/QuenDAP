af7d94007bc512c39d46b6e03ca555b26780b58c
Carrera Groba <abraham.carrera.groba@xunta.es>
2023-09-29 14:19:59 +0200

    project structure

---
+++ src/main/java/com/quendap/quendap/QuendapApplication.java
@@ -0,0 +1,13 @@
+package com.quendap.quendap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuendapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuendapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quendap/quendap/QuendapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quendap.quendap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuendapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
