64fe8f9976d4f48917babce4abf9b7b8d14cea27
Abraham <abraham@MacBook-Pro-de-Abraham.local>
2023-10-15 21:03:03 +0200

    swagger added

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ run:
 	mvn compile
 	mvn spring-boot:stop
 	mvn spring-boot:start
+stop:
+	mvn spring-boot:stop
--- src/main/java/com/quendap/quendap/QuendapApplication.java
+++ src/main/java/com/quendap/quendap/QuendapApplication.java
@@ -3,6 +3,7 @@ package com.quendap.quendap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class QuendapApplication {
 
